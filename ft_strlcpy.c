bc5036e4eebf00cc546b5052bffad296c8c2775b
wladimir-schaefer <wladimir.schaefer.de@gmail.com>
2025-04-27 13:31:13 +0200

    Implement ft_strlcpy function and update header file to include its declaration

---
+++ ft_strlcpy.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dsize)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	i = 0;
+	if (dsize > 0)
+	{
+		while (src[i] != '\0' && i < dsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+
+	return (src_len);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	char src[] = "Hello, World!";
+	char dst[20];
+	
+	size_t len1 = ft_strlcpy(dst, src, sizeof(dst));
+	printf("dst: '%s', len: %zu\n", dst, len1);
+
+	size_t len2 = ft_strlcpy(dst, src, 5);
+	printf("dst: '%s', len: %zu\n", dst, len2);	
+
+	return 0;
+}
+*/
--- libft.h
+++ libft.h
@@ -1,7 +1,7 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
-#include <stddef.h> // needed for using size_t
+#include <stddef.h>
 
 int ft_isalpha(int c);
 int	ft_isdigit(int c);
@@ -12,6 +12,8 @@ int	ft_strlen(char *str);
 void *ft_memset(void *s, int c, size_t n);
 void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
+size_t ft_strlcpy(char *dest, const char *src, size_t dsize);
 
 
 #endif /* LIBFT_H */
