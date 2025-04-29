1c96314f27838089d1e799e71a6f4b2408bd3ddb
wladimir-schaefer <wladimir.schaefer.de@gmail.com>
2025-04-29 16:38:58 +0200

    Implement ft_strlcat function and update header file to include its declaration

--- libft.h
+++ libft.h
@@ -14,6 +14,6 @@ void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy(char *dest, const char *src, size_t dsize);
-
+size_t ft_strlcat(char *dest, const char *src, size_t dsize);
 
 #endif /* LIBFT_H */
