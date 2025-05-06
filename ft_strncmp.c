1e9388d27fd08a43b4a1d9cab3a481f74153a0f3
wladimir-schaefer <wladimir.schaefer.de@gmail.com>
2025-05-06 09:22:45 +0200

    Implement ft_strncmp function and add a test in main for validation

---
+++ ft_strncmp.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while(i < n - 1 && s1[i] != '\0' && s2[i] != '\0')
+	{
+		if(s1[i] != s2[i])
+			return((int)(s1[i] - s2[i]));
+		else
+			i++;
+	}
+	return(0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	const char *str1 = "aba";
+	const char *str2 = "abc";
+	printf("%d\n", ft_strncmp(str1, str2, 3));
+}
