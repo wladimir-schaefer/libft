c7f62e23941df569bf4c8802c31db00906169d9a
wschafer <wschafer@student.42berlin.de>
2025-05-27 10:43:29 +0200

    Implement ft_lstclear function and update header file to include its declaration

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wschafer <wschafer@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/26 15:58:37 by wschafer          #+#    #+#             */
-/*   Updated: 2025/05/26 15:58:41 by wschafer         ###   ########.fr       */
+/*   Created: 2025/05/27 09:58:09 by wschafer          #+#    #+#             */
+/*   Updated: 2025/05/27 09:58:14 by wschafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*temp;
+
 	if (!lst || !del)
 		return ;
-	del(lst->content);
-	free(lst);
+
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = temp;
+	}
+	*lst = NULL;
 }
